66395a564c1ad0db56258c6ac0855877927d0f13
albermartin97 <albermartin97@gmail.com>
2019-07-03 12:21:00 +0200

    Actualizacion para intentar ejecutar test en travis

--- src/test/TestExamenes.java
+++ src/test/TestExamenes.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package UnitTestExamenes;
+package test;
 
 import java.math.BigDecimal;
 import java.util.List;
